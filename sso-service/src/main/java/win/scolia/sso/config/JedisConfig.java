f6c5cd1ef63b4e0f1d53fc2c938d0d4626083528
scolia <scolia@qq.com>
2017-11-29 00:53:34 +0800

    集成redis, 完成登录功能

---
+++ sso-service/src/main/java/win/scolia/sso/config/JedisConfig.java
@@ -0,0 +1,7 @@
+package win.scolia.sso.config;
+
+/**
+ * Created by scolia on 2017/11/28
+ */
+public class JedisConfig {
+}
---
+++ sso-service/src/main/java/win/scolia/sso/utils/TokenUtils.java
@@ -0,0 +1,7 @@
+package win.scolia.sso.utils;
+
+/**
+ * Created by scolia on 2017/11/28
+ */
+public class TokenUtils {
+}
